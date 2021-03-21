c49b23bcaac7c619be2160c9f8408f2dbf44a791
fir2017 <razvantrufin99x@gmail.com>
2021-03-21 02:44:14 +0200

    Add files via upload

--- cadfrm.cs
+++ cadfrm.cs
@@ -18,10 +18,13 @@ namespace caddotnet
         public bool ismd = false;
         public int lastx = 0;
         public int lasty = 0;
-
+        public string mode = "NONE";
+        public void writemode(string s) { mode = s; }
+        public Form1 frm;
         public cadfrm()
         {
             InitializeComponent();
+           
         }
 
         private void cadfrm_MouseDown(object sender, MouseEventArgs e)
@@ -32,14 +35,32 @@ namespace caddotnet
         private void cadfrm_MouseMove(object sender, MouseEventArgs e)
         {
             
-
-            if (ismd == true)
+            try
             {
-               // g.DrawEllipse(p, e.X, e.Y, 2, 2);
-                g.DrawLine(p, lastx, lasty, e.X, e.Y);
+                mode = frm.getthemode();
+                if (ismd == true)
+                {
+                     if (mode == "NONE")
+                    {
+                        g.DrawEllipse(p, e.X, e.Y, 1, 1);
+                    }
+                    else if (mode == "DOT")
+                    {
+
+                        g.DrawEllipse(p, e.X, e.Y, 2, 2);
+
+                    }
+                    else if (mode == "LINE")
+                    {
+                        g.DrawLine(p, lastx, lasty, e.X, e.Y);
+                    }
+                }
+                lastx = e.X;
+                lasty = e.Y;
             }
-            lastx = e.X;
-            lasty = e.Y;
+            catch { }
+
+
         }
 
         private void cadfrm_MouseUp(object sender, MouseEventArgs e)
@@ -49,7 +70,10 @@ namespace caddotnet
 
         private void cadfrm_Load(object sender, EventArgs e)
         {
-
+            try {
+                frm = (Form1)this.ParentForm;
+            }
+            catch { }
         }
 
         private void cadfrm_Shown(object sender, EventArgs e)
@@ -58,7 +82,7 @@ namespace caddotnet
             f = this.Font;
             b = new SolidBrush(Color.Black);
             p = new Pen(Color.Black);
-
+          
         }
     }
 }
